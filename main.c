e547fdcca1835f871e9c89cc2120ad946700dbc2
brenda-brito <134876463+brenda-brito@users.noreply.github.com>
2025-08-13 11:17:25 -0300

    IMC_BrendaBrito

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Cálculo do IMC\n");
+    printf("Altura %f\n", 1.64);
+    printf("Peso %f\n", 62.5);
+    
+    printf("IMC: %f\n", 62.5/(1.64*1.64));
+}
